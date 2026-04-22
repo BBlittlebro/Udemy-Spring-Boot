9f8fd48bd8e47bd6051587743579c7f70d4e9154
BBlittlebro <popo8810017269@gmail.com>
2026-04-22 17:30:11 +0800

    79. 練習使用 DAO 來讀取所有資料，findAll

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -23,8 +25,21 @@ public class CruddemoApplication {
 
             // createMultipleStudents(studentDAO);
 
-            readStudent(studentDAO);
+            // readStudent(studentDAO);
+
+            queryForStudents(studentDAO);
         };
+    }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+
+        // get a list of students
+        List<Student> theResults = studentDAO.findAll();
+
+        // display list of students
+        for (Student tempStudent : theResults) {
+            System.out.println(tempStudent);
+        }
     }
 
     private void readStudent(StudentDAO studentDAO) {
