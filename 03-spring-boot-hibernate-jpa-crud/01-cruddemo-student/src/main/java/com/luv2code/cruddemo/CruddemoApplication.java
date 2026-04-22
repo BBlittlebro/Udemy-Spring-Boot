070859ec9123d78cc30c2ea0c2ce02b45746e1c1
BBlittlebro <popo8810017269@gmail.com>
2026-04-22 15:18:21 +0800

    74. 練習使用 DAO 來儲存多個 student

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -19,8 +19,25 @@ public class CruddemoApplication {
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
         return runner -> {
-            createStudent(studentDAO);
+            // createStudent(studentDAO);
+
+            createMultipleStudents(studentDAO);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+
+        // create multiple students
+        System.out.println("Creating 3 student objects ...");
+        Student tempStudent1 = new Student("Jacky", "Lin", "Jacky@gmail.com");
+        Student tempStudent2 = new Student("Tom", "Chen", "Tom@gmail.com");
+        Student tempStudent3 = new Student("Elsa", "Lu", "Elsa@gmail.com");
+
+        // save the student objects
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
     }
 
     private void createStudent(StudentDAO studentDAO) {
