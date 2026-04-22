8e95a0b1844ddd5ae275a61cecb42f6417e9c76d
BBlittlebro <popo8810017269@gmail.com>
2026-04-22 18:19:36 +0800

    84. 練習使用 DAO 來刪除多筆資料

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -33,8 +33,17 @@ public class CruddemoApplication {
 
             // updateStudent(studentDAO);
 
-            deleteStudent(studentDAO);
+            // deleteStudent(studentDAO);
+
+            deleteAllStudents(studentDAO);
         };
+    }
+
+    private void deleteAllStudents(StudentDAO studentDAO) {
+
+        System.out.println("Deleting all students");
+        int numRowsDeleted = studentDAO.deleteAll();
+        System.out.println("Deleting row count: " + numRowsDeleted);
     }
 
     private void deleteStudent(StudentDAO studentDAO) {
