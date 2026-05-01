88b837958b05581bccf7a19831d643394a2a9f89
BBlittlebro <popo8810017269@gmail.com>
2026-05-01 21:41:41 +0800

    251. 練習登入時，導向自己做的登入頁，此篇章很多東西都要命名一致 or 預設，要注意

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +33,22 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer.
+                                anyRequest().authenticated()                // 要求所有 request 都要先過登入頁
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser") // Spring 會自動幫我抓，不用寫 Controller
+                                .permitAll()                                // 允許所有人訪問豋入頁
+                );
+
+        return http.build();
     }
 }
