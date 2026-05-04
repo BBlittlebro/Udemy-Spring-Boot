ff2724eabb7b1f7373d8721e2d5e95790d8fb9fd
BBlittlebro <popo8810017269@gmail.com>
2026-05-04 21:08:48 +0800

    308. 練習使用 @OneToMany Mapping，新增程式

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,90 @@
+package com.luv2code.cruddemo;
+
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+
+        return runner -> {
+            // createInstructor(appDAO);
+            // findInstructor(appDAO);
+            // deleteInstructor(appDAO);
+            // findInstructorDetail(appDAO);
+            deleteInstructorDetail(appDAO);
+        };
+    }
+
+    private void deleteInstructorDetail(AppDAO appDAO) {
+
+        int theId = 3;
+        System.out.println("Deleting instructorDetail id: " + theId);
+
+        appDAO.deleteInstructorDetailById(theId);
+        System.out.println("Done!");
+    }
+
+    private void findInstructorDetail(AppDAO appDAO) {
+
+        int theId = 2;
+        System.out.println("Finding instructorDetail id: " + theId);
+
+        InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+        System.out.println("instructorDetail only: " + tempInstructorDetail);
+        System.out.println("the associated tempInstructor: " + tempInstructorDetail.getInstructor());
+        System.out.println("Done!");
+    }
+
+    private void deleteInstructor(AppDAO appDAO) {
+
+        int theId = 1;
+        System.out.println("Deleting instructor id: " + theId);
+
+        appDAO.deleteInstructorById(theId);
+        System.out.println("Done!");
+    }
+
+    private void findInstructor(AppDAO appDAO) {
+
+        int theId = 2;
+        System.out.println("Finding instructor id: " + theId);
+
+        Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+        System.out.println("tempInstructor: " + tempInstructor);
+        System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+
+//        Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+//        InstructorDetail tempInstructorDetail = new InstructorDetail(
+//                "http://www.luv2coe.com/youtube",
+//                "Luv 2 code!!!");
+
+        Instructor tempInstructor = new Instructor("Andy", "Wu", "andy@luv2code.com");
+        InstructorDetail tempInstructorDetail = new InstructorDetail(
+                "http://www.luv2coe.com/youtube",
+                "Gaming");
+
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+        // NOTE: this will ALSO save the details object because of CascadeType.ALL
+        System.out.println("Saving instructor:" + tempInstructor);
+        appDAO.save(tempInstructor);
+        System.out.println("Done!");
+    }
+}
